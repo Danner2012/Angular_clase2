910e229e7372e8e5b55f5e614891ff8806315e4d
Danner2012 <dannerpantoja@gmail.com>
2026-02-18 20:15:53 -0400

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+import { EditorFiltroComponent } from './editor-filtro/editor-filtro'; 
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [EditorFiltroComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
